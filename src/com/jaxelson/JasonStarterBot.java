6fe40a0d91ccab6f854597f0c6919e6b52e9a8a6
Jason Axelson (imac) <jason.axelson@gmail.com>
2010-04-07 00:14:18 -1000

    Did some initial playing around

--- src/com/jaxelson/JasonStarterBot.java
+++ src/com/jaxelson/JasonStarterBot.java
@@ -1,21 +1,79 @@
 package com.jaxelson;
 
+import java.awt.Graphics2D;
+
 import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
 
 
 public class JasonStarterBot extends AdvancedRobot {
+	private Integer debug = 1;
+	private Integer state = 0;
+	
 	public void run() {
+		setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+//		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
         while (true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+        	
+//        	System.out.println("Looping!");
+        	if(debug >= 1) {
+        		printMyInfo();
+        	}
+        	switch(state) {
+        	case 0:	// Do a loop
+        		System.out.println("State 0");
+        		setMaxVelocity(5);
+        		setAhead(600);
+        		setTurnRight(360);
+        		state++;
+        		break;
+        	case 1:
+        		System.out.println("State 1");
+        		setFire(1.0);
+        		if(getDistanceRemaining() <= 0) {
+        			state++;
+        		}
+        		break;
+        	case 2:
+        		//setTurnGunRightRadians(3);
+        	}
+            
+//            waitFor(new TurnCompleteCondition(this));
+        	scan();
+            execute();
+//            setAhead(-100);
+            //setTurnGunRight(360);
+            //setBack(100);
+            //setTurnGunRight(360);
+            // Check for new targets.
+            // Only necessary for Narrow Lock because sometimes our radar is already
+            // pointed at the enemy and our onScannedRobot code doesn't end up telling
+            // it to turn, so the system doesn't automatically call scan() for us
+            // [see the javadocs for scan()].
+            scan();
         }
     }
  
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+    	if(debug >= 2) {
+    		System.out.println("Found robot: "+ e.getName());
+    		printRobot(e);
+    	}
+    	double radarTurn =
+    		// Absolute bearing to target
+    		getHeadingRadians() + e.getBearingRadians()
+    		// Subtract current radar heading to get turn required
+    		- getRadarHeadingRadians();
+
+    	setTurnRadarRightRadians(2.0 * Utils.normalRelativeAngle(radarTurn));
+    	
+    	double gunTurn = getHeadingRadians() + e.getBearingRadians() - getGunHeadingRadians();
+    	if(state >= 1) {
+    		setTurnGunRightRadians(Utils.normalRelativeAngle(gunTurn));
+    	}
+    	setFire(1.0);
     }
     
     /**
