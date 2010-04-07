17643efaf8d1badcbfe77aefe44031bc39f1fd16
Jason Axelson (imac) <jason.axelson@gmail.com>
2010-04-06 23:22:03 -1000

    Created printRobot function

--- src/com/jaxelson/JasonStarterBot.java
+++ src/com/jaxelson/JasonStarterBot.java
@@ -17,4 +17,18 @@ public class JasonStarterBot extends AdvancedRobot {
     public void onScannedRobot(ScannedRobotEvent e) {
         fire(1);
     }
+    
+    public void printRobot(ScannedRobotEvent e) {
+    	System.out.println("Name: "+ e.getName());
+    	System.out.println("Bearing: "+ e.getBearing());
+    	System.out.println("Bearing (radians): "+ e.getBearingRadians());
+    	System.out.println("Distance: "+ e.getDistance());
+    	System.out.println("Energy: "+ e.getEnergy());
+    	System.out.println("Heading: "+ e.getHeading());
+    	System.out.println("Heading (radians)"+ e.getHeadingRadians());
+    	System.out.println("Velocity: "+ e.getVelocity());
+    	System.out.println("Time: "+ e.getTime());
+    	System.out.println("Priority: "+ e.getPriority());
+    }
+    
 }
