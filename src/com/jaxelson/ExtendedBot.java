29e7bf67c3d82693e3e174da315e0f987d44c91c
Jason Axelson <jason.axelson@gmail.com>
2010-04-10 18:48:33 -1000

    Moved some easy functions to ExtendedBot

--- src/com/jaxelson/ExtendedBot.java
+++ src/com/jaxelson/ExtendedBot.java
@@ -1,9 +1,23 @@
 package com.jaxelson;
 
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+
 import robocode.AdvancedRobot;
 
 public class ExtendedBot extends AdvancedRobot {
-	public Double testGetX() {
-		return new Double(this.getX());
-	}
+
+    public void drawCenteredCircle(Graphics2D g, Integer radius) {
+    	double x = getX() - radius;
+        double y = getY() - radius;
+		Shape circle = new Ellipse2D.Double(x, y, radius*2, radius*2);
+        g.draw(circle);
+    }
+    
+    public Double getDistanceToRightWall() {
+    	double botRightEdge = getX();
+    	double rightWallLocation = getBattleFieldWidth();
+    	return rightWallLocation - botRightEdge;
+    }
 }
--- src/com/jaxelson/JasonStarterBot.java
+++ src/com/jaxelson/JasonStarterBot.java
@@ -2,8 +2,6 @@ package com.jaxelson;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.geom.Ellipse2D;
 
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
@@ -125,7 +123,6 @@ public class JasonStarterBot extends ExtendedBot {
      */
     public void printMyInfo() {
     	System.out.println("Heading: "+ this.getHeading());
-    	System.out.println("testGetX: "+ this.testGetX());
     	System.out.println("Heading (radians): "+ this.getHeadingRadians());
     	System.out.println("Distance remaining: "+ this.getDistanceRemaining());
     }
@@ -140,18 +137,5 @@ public class JasonStarterBot extends ExtendedBot {
         
         drawCenteredCircle(g, 200);
         
-    }
-    
-    public void drawCenteredCircle(Graphics2D g, Integer radius) {
-    	double x = getX() - radius;
-        double y = getY() - radius;
-		Shape circle = new Ellipse2D.Double(x, y, radius*2, radius*2);
-        g.draw(circle);
-    }
-    
-    public Double getDistanceToRightWall() {
-    	double botRightEdge = getX();
-    	double rightWallLocation = getBattleFieldWidth();
-    	return rightWallLocation - botRightEdge;
     }
 }
