ea34308b9f7d5741a0169f294049a457eafb80c3
Jason Axelson (imac) <jason.axelson@gmail.com>
2010-04-18 18:26:45 -1000

    Added linear targeting and head on targeting

--- src/com/jaxelson/ExtendedBotUH.java
+++ src/com/jaxelson/ExtendedBotUH.java
@@ -1,6 +1,7 @@
 package com.jaxelson;
 
 import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
 
 import robocode.ScannedRobotEvent;
 import robocode.TeamRobot;
@@ -172,5 +173,49 @@ public class ExtendedBotUH extends TeamRobot {
 	    } else {
 	        return trimmedAngle;
 	    }
+	}
+	
+	public void headOnTargeting(EnemyBot target, double firePower) {
+		this.turnGunTo(target);
+        this.setFire(firePower);
+	}
+	
+	public void linearTargeting(ScannedRobotEvent e) {
+		double bulletPower = Math.min(3.0,getEnergy());
+		double myX = getX();
+		double myY = getY();
+		double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
+		double enemyX = getX() + e.getDistance() * Math.sin(absoluteBearing);
+		double enemyY = getY() + e.getDistance() * Math.cos(absoluteBearing);
+		double enemyHeading = e.getHeadingRadians();
+		double enemyVelocity = e.getVelocity();
+
+
+		double deltaTime = 0;
+		double battleFieldHeight = getBattleFieldHeight(), 
+		       battleFieldWidth = getBattleFieldWidth();
+		double predictedX = enemyX, predictedY = enemyY;
+		while((++deltaTime) * (20.0 - 3.0 * bulletPower) < 
+		      Point2D.Double.distance(myX, myY, predictedX, predictedY)){		
+			predictedX += Math.sin(enemyHeading) * enemyVelocity;	
+			predictedY += Math.cos(enemyHeading) * enemyVelocity;
+			if(	predictedX < 18.0 
+				|| predictedY < 18.0
+				|| predictedX > battleFieldWidth - 18.0
+				|| predictedY > battleFieldHeight - 18.0){
+				predictedX = Math.min(Math.max(18.0, predictedX), 
+		                    battleFieldWidth - 18.0);	
+				predictedY = Math.min(Math.max(18.0, predictedY), 
+		                    battleFieldHeight - 18.0);
+				break;
+			}
+		}
+		double theta = Utils.normalAbsoluteAngle(Math.atan2(
+		    predictedX - getX(), predictedY - getY()));
+
+		setTurnRadarRightRadians(
+		    Utils.normalRelativeAngle(absoluteBearing - getRadarHeadingRadians()));
+		setTurnGunRightRadians(Utils.normalRelativeAngle(theta - getGunHeadingRadians()));
+		fire(bulletPower);
 	}
 }
